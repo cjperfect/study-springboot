5ac80db6edfeadd3fc62ff9059c44f6c3e1f58ce
cjperfect <1982145923@qq.com>
2023-08-01 16:33:15 +0800

    feat: 编写带有path参数的get接口

--- src/main/java/com/cj/project/controller/indexController.java
+++ src/main/java/com/cj/project/controller/indexController.java
@@ -1,16 +1,13 @@
 package com.cj.project.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api")
 public class indexController {
-    @GetMapping("")
-    public String index(@RequestParam String name) {
+    @GetMapping("/{name}")
+    public String index(@PathVariable("name") String name) {
         return "hello => " + name;
     }
 }
