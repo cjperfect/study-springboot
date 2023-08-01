72a171e0308b6cd820ba3b92c4c17376b785003a
cjperfect <1982145923@qq.com>
2023-08-01 16:27:15 +0800

    feat: 编写带有query参数的get接口

--- src/main/java/com/cj/project/controller/indexController.java
+++ src/main/java/com/cj/project/controller/indexController.java
@@ -2,6 +2,7 @@ package com.cj.project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class indexController {
     @GetMapping("")
-    public String index() {
-        return "chenjiang";
+    public String index(@RequestParam String name) {
+        return "hello => " + name;
     }
 }
